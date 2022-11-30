7a70daa61fe2ab16e2100a001eca2ddb2d5f108f
Davi Pereira <58438668+Pebone@users.noreply.github.com>
2022-11-30 00:48:05 -0300

    Update graphQL

--- node-graphql/index.js
+++ node-graphql/index.js
@@ -60,21 +60,21 @@ const typeDefs = gql`
     editProfessor(id: ID!, nome: String!): Professores
     deleteProfessor(id: ID!): DeleteResponse
 
-    addDisciplina(nome: String!): Disciplinas
-    editDisciplina(id: ID!, nome: String!): Disciplinas
+    addDisciplina(nome: String!, horario: String!): Disciplinas
+    editDisciplina(id: ID!, nome: String!, horario: String!): Disciplinas
     deleteDisciplina(id: ID!): DeleteResponse
 
-    addAluno(nome: String!): Alunos
-    editAluno(id: ID!, nome: String!): Alunos
-    deleteAluno(id: ID!): DeleteResponse
+    addAluno(nome: String!, matricula: String!): Alunos
+    editAluno(nome: String!, matricula: String!): Alunos
+    deleteAluno(nome: String!, matricula: String!): DeleteResponse
 
-    addDisciplinasProfessor(nome: String!): DisciplinasProfessores
-    editDisciplinasProfessor(id: ID!, nome: String!): DisciplinasProfessores
-    deleteDisciplinasProfessor(id: ID!): DeleteResponse
+    addDisciplinasProfessor(idDisciplina: ID!, idProfessor: String!): DisciplinasProfessores
+    editDisciplinasProfessor(idDisciplina: ID!, idProfessor: String!): DisciplinasProfessores
+    deleteDisciplinasProfessor(idDisciplina: ID!): DeleteResponse
 
-    addDisciplinasAluno(nome: String!): DisciplinasAlunos
-    editDisciplinasAluno(id: ID!, nome: String!): DisciplinasAlunos
-    deleteDisciplinasAluno(id: ID!): DeleteResponse
+    addDisciplinasAluno(idDisciplina: ID!, idAluno: String!): DisciplinasAlunos
+    editDisciplinasAluno(idDisciplina: ID!, idAluno: String!): DisciplinasAlunos
+    deleteDisciplinasAluno(idDisciplina: ID!): DeleteResponse
 
   }
 
@@ -89,8 +89,10 @@ const typeDefs = gql`
 console.log(db)
 
 const professores = db.Professores;
-
-console.log(professores)
+const alunos = db.Alunos;
+const disciplinas = db.Disciplinas;
+const disciplinasAlunos = db.DisciplinasAlunos;
+const disciplinasProfessores = db.DisciplinasProfessores;
 
 const addProfessor = (professor) => {
 
@@ -99,11 +101,22 @@ const addProfessor = (professor) => {
   
 };
 
+const addDisciplina = (disciplina) => {
+
+  const id = uuid();
+  return (disciplinas[id] = { ...disciplina, id });
+  
+};
+
 const resolvers = {
 
   Query: {
 
     professores: () => Object.values(professores),
+    alunos: () => Object.values(alunos),
+    disciplinas: () => Object.values(disciplinas),
+    disciplinasProfessores: () => Object.values(disciplinasProfessores),
+    disciplinasAlunos: () => Object.values(disciplinasAlunos),
 
   },
 
@@ -125,6 +138,82 @@ const resolvers = {
     deleteProfessor: async (parent, { id }) => {
       const ok = Boolean(professores[id]);
       delete professores[id];
+      return { ok };
+    },
+
+    addDisciplina: async (parent, disciplina) => {
+      return addDisciplina(disciplina);
+    },
+    editDisciplina: async (parent, { id, ...disciplina }) => {
+      if (!disciplinas[id]) {
+        throw new Error("Disciplina doesn't exist");
+      }
+      disciplinas[id] = {
+        ...disciplinas[id],
+        ...disciplina,
+      };
+      return disciplinas[id];
+    },
+    deleteDisciplina: async (parent, { id }) => {
+      const ok = Boolean(disciplinas[id]);
+      delete disciplinas[id];
+      return { ok };
+    },
+
+    addAluno: async (parent, aluno) => {
+      return addAluno(aluno);
+    },
+    editAluno: async (parent, { id, ...aluno }) => {
+      if (!alunos[id]) {
+        throw new Error("Aluno doesn't exist");
+      }
+      alunos[id] = {
+        ...alunos[id],
+        ...aluno,
+      };
+      return professores[id];
+    },
+    deleteAluno: async (parent, { id }) => {
+      const ok = Boolean(alunos[id]);
+      delete alunos[id];
+      return { ok };
+    },
+
+    addDisciplinasProfessor: async (parent, disciplinasProfessor) => {
+      return addDisciplinasProfessor(disciplinasProfessor);
+    },
+    editDisciplinasProfessor: async (parent, { id, ...disciplinasProfessor }) => {
+      if (!disciplinasProfessores[id]) {
+        throw new Error("DisciplinasProfessores doesn't exist");
+      }
+      disciplinasProfessores[id] = {
+        ...disciplinasProfessores[id],
+        ...disciplinasProfessor,
+      };
+      return disciplinasProfessores[id];
+    },
+    deleteDisciplinasProfessor: async (parent, { id }) => {
+      const ok = Boolean(disciplinasProfessores[id]);
+      delete disciplinasProfessores[id];
+      return { ok };
+    },
+
+    addDisciplinasAluno: async (parent, disciplinasAluno) => {
+      return addDisciplinasAluno(disciplinasAluno);
+    },
+    editDisciplinasAluno: async (parent, { id, ...disciplinasAluno }) => {
+      if (!disciplinasAlunos[id]) {
+        throw new Error("DisciplinasAlunos doesn't exist");
+      }
+      disciplinasAlunos[id] = {
+        ...disciplinasAlunos[id],
+        ...disciplinasAluno,
+      };
+      return disciplinasAlunos[id];
+    },
+    deleteDisciplinasAluno: async (parent, { id }) => {
+      const ok = Boolean(disciplinasAlunos[id]);
+      delete disciplinasAlunos[id];
       return { ok };
     },
 
