8098a3876c972edd7e0bf17181dc5b5020e52b74
Victor Torres <victor.tmeloo@gmail.com>
2022-12-03 19:24:12 -0300

    Mais endpoints REST

--- node-rest/api/controllers/msgController.js
+++ node-rest/api/controllers/msgController.js
@@ -2,8 +2,6 @@
 
 const database = require('../../../database.json')
 
-// GETs
-
 exports.listarTudo = function (req, res) {
    try {
       res.json(database)
@@ -41,6 +39,50 @@ exports.criarProfessor = function (req, res) {
    }
 };
 
+exports.editarProfessor = function (req, res) {
+
+   const nome = req.body.nome
+   const professor = req.body;
+
+   try {
+
+      database.Professores.forEach((professor) => {
+         if (professor.nome === nome) {
+            professor = req.body
+         } else {
+            res.json("Professor não encontrado")
+         }
+      })
+
+      res.json(professor)
+
+   } catch (error) {
+      res.json(error)
+   }
+};
+
+exports.deletarProfessor = function (req, res) {
+
+   const nome = req.body.nome
+
+   try {
+
+      database.Professores.forEach((professor) => {
+         if (professor.nome === nome) {
+            database.Professores.splice(database.Professores.indexOf(professor), 1)
+         } else {
+            res.json("Professor não encontrado")
+         }
+      })
+
+      res.json(database.Professores)
+
+   } catch (error) {
+      res.json(error)
+   }
+
+};
+
 // Alunos
 
 exports.listarAlunos = function (req, res) {
@@ -67,6 +109,51 @@ exports.criarAluno = function (req, res) {
    catch (error) {
       res.json(error)
    }
+};
+
+exports.editarAluno = function (req, res) {
+
+   const nome = req.body.nome
+   const aluno = req.body;
+
+   try {
+
+      database.Alunos.forEach((aluno) => {
+         if (aluno.nome === nome) {
+            aluno = req.body
+         } else {
+            res.json("Aluno não encontrado")
+         }
+      })
+
+      res.json(aluno)
+
+   } catch (error) {
+      res.json(error)
+   }
+
+};
+
+exports.deletarAluno = function (req, res) {
+
+   const nome = req.body.nome
+
+   try {
+
+      database.Alunos.forEach((aluno) => {
+         if (aluno.nome === nome) {
+            database.Alunos.splice(database.Alunos.indexOf(aluno), 1)
+         } else {
+            res.json("Aluno não encontrado")
+         }
+      })
+
+      res.json(database.Alunos)
+
+   } catch (error) {
+      res.json(error)
+   }
+
 };
 
 exports.listarAlunosDeDisciplina = function (req, res) {
@@ -128,6 +215,51 @@ exports.criarDisciplina = function (req, res) {
    catch (error) {
       res.json(error)
    }
+};
+
+exports.editarDisciplina = function (req, res) {
+
+   const nome = req.body.nome
+   const disciplina = req.body;
+
+   try {
+
+      database.Disciplinas.forEach((disciplina) => {
+         if (disciplina.nome === nome) {
+            disciplina = req.body
+         } else {
+            res.json("Disciplina não encontrada")
+         }
+      })
+
+      res.json(disciplina)
+
+   } catch (error) {
+      res.json(error)
+   }
+
+};
+
+exports.deletarDisciplina = function (req, res) {
+
+   const nome = req.body.nome
+
+   try {
+
+      database.Disciplinas.forEach((disciplina) => {
+         if (disciplina.nome === nome) {
+            database.Disciplinas.splice(database.Disciplinas.indexOf(disciplina), 1)
+         } else {
+            res.json("Disciplina não encontrada")
+         }
+      })
+
+      res.json(database.Disciplinas)
+
+   } catch (error) {
+      res.json(error)
+   }
+
 };
 
 exports.listarDisciplinasDeAluno = function (req, res) {
@@ -193,27 +325,4 @@ exports.listarDisciplinasDeProfessor = function (req, res) {
    })
 
    res.json(disciplinas)
-};
-
-
-// PUT
-
-exports.update_a_message = function (req, res) {
-   database.findOneAndUpdate({ _id: req.params.msgId }, req.body, { new: true }, function (err, msg) {
-      if (err)
-         res.send(err);
-      res.json(msg);
-   });
-};
-
-// DELETE
-
-exports.delete_a_message = function (req, res) {
-   database.remove({
-      _id: req.params.msgId
-   }, function (err, msg) {
-      if (err)
-         res.send(err);
-      res.json({ message: 'Message successfully deleted' });
-   });
 };
--- node-rest/api/routes/msgRoutes.js
+++ node-rest/api/routes/msgRoutes.js
@@ -6,18 +6,30 @@ module.exports = function (app) {
 
    app.route('/')
       .get(controller.listarTudo);
-   
+
    app.route('/professores')
       .get(controller.listarProfessores)
       .post(controller.criarProfessor);
+
+   app.route('/professores/:nome')
+      .put(controller.editarProfessor)
+      .delete (controller.deletarProfessor);
 
    app.route('/alunos')
       .get(controller.listarAlunos)
       .post(controller.criarAluno);
 
+   app.route('/alunos/:nome')
+      .put(controller.editarAluno)
+      .delete(controller.deletarAluno);
+
    app.route('/disciplinas')
       .get(controller.listarDisciplinas)
       .post(controller.criarDisciplina);
+
+   app.route('/disciplinas/:nome')
+      .put(controller.editarDisciplina)
+      .delete (controller.deletarDisciplina);
 
    app.route('/alunosDaDisciplina/:nome')
       .get(controller.listarAlunosDeDisciplina)
